fb8aeae5a2573c76a34789ded9c6e91c6cd83d73
Sraav Gamer <sraav.3@gmail.com>
2021-08-05 20:11:18 +0530

    first

--- shoot.js
+++ shoot.js
@@ -14,37 +14,53 @@ AFRAME.registerComponent("bullets", {
 
         bullet.setAttribute("material", "color", "black");
 
-        var cam = document.querySelector("#camera");
+        var cam = document.querySelector("#camera-rig");
+
         pos = cam.getAttribute("position");
 
         bullet.setAttribute("position", {
           x: pos.x,
-          y: pos.y,
-          z: pos.z,
+          y: pos.y + 1.5,
+          z: pos.z - 0.5,
         });
 
         var camera = document.querySelector("#camera").object3D;
+
+        //get the camera direction as Three.js Vector
         var direction = new THREE.Vector3();
         camera.getWorldDirection(direction);
-        bullet.setAttribute("velocity", direction.multiplyScalar(-30));
+
+        //set the velocity and it's direction
+        bullet.setAttribute("velocity", direction.multiplyScalar(-20));
+
         var scene = document.querySelector("#scene");
+
+        //set the bullet as the dynamic entity
         bullet.setAttribute("dynamic-body", {
           shape: "sphere",
           mass: "0",
         });
         bullet.setAttribute("visible", false);
+
+        //add the collide event listener to the bullet
         bullet.addEventListener("collide", this.removeBullet);
+
         scene.appendChild(bullet);
 
+        //shooting sound
         this.shootSound();
       }
     });
   },
   removeBullet: function (e) {
     var scene = document.querySelector("#scene");
+    //bullet element
     var element = e.detail.target.el;
+
+    //element which is hit
     var elementHit = e.detail.body.el;
 
+    //Create paint splash
     var paint = document.createElement("a-entity");
     var pos = element.getAttribute("position")
     var rotate = elementHit.getAttribute("rotation")
@@ -80,8 +96,11 @@ AFRAME.registerComponent("bullets", {
       height: 0.5
     });
     scene.appendChild(paint)
+
+    //remove event listener
     element.removeEventListener("collide", this.removeBullet);
-    
+
+    //remove the bullets from the scene      
     scene.removeChild(element);
   },
   shootSound: function () {
@@ -89,4 +108,3 @@ AFRAME.registerComponent("bullets", {
     entity.components.sound.playSound();
   },
 });
-
